4e5820daf5ad9541514c5373a959b88bc13b46be
jjy990412 <wjdwn282@gmail.com>
2023-12-29 16:58:39 +0900

    memberentity추가

--- src/main/java/com/ll/medium/global/security/config/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/config/SecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -32,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(
                         sessionManagement -> sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
