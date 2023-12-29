403b99774b0c24abfe54a9712565929cf98321a6
jjy990412 <wjdwn282@gmail.com>
2023-12-29 17:21:21 +0900

    Role 추가

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -2,12 +2,19 @@ package com.ll.medium.domain.member.member.entity;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.ll.medium.domain.member.role.entity.Role;
 import com.ll.medium.global.jpa.entity.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -32,4 +39,8 @@ public class Member extends BaseEntity {
     private boolean isPaid;
 
     private String password;
+
+        @Builder.Default
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Role> roles = new HashSet<>();
 }
