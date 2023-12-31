08d4ab89721288f158f99363cb8f6e16c1d91344
jjy990412 <wjdwn282@gmail.com>
2023-12-31 17:37:09 +0900

    navbar 추가

---
+++ frontapp/components/MediumNavbar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+import { instance } from "@/config/axiosConfig";
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
+import { logoutAsync } from "@/store/userSlice";
+
+const MediumNavbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+  const router = useRouter();
+  const dispatch = useDispatch<AppDispatch>();
+
+  const logoutF = () => {
+    // 로그아웃 로직
+    dispatch(logoutAsync()).then(() => {
+        alert("로그아웃 되었습니다.");
+        router.push("/");
+    })
+  };
+
+  const menuItems = [
+    { name: "글 등록", href: "/post/write", permission: "auth" },
+    { name: "전체 글", href: "/post/list", permission: "all" },
+    { name: "나의 글", href: "/post/mylist", permission: "auth" },
+    { name: "Sign Up", href: "/signup", permission: "anonymous" },
+    { name: "LogIn", href: "/login", permission: "anonymous" },
+    { name: "Logout", href: "#", permission: "auth" },
+  ];
+
+  // 필터링 로직을 별도의 함수로 분리
+  const filteredMenuItems = menuItems.filter(
+    (item) =>
+      (isLoggedIn && item.permission === "auth") ||
+      (!isLoggedIn && item.permission === "anonymous") ||
+      item.permission === "all",
+  );
+
+  // 메뉴 아이템 렌더링을 위한 컴포넌트
+  const MenuItem = ({ item }: any) => (
+    <NavbarMenuItem className="text-sm hover:text-blue-600">
+      <Link
+        className="w-full"
+        href={item.href}
+        onClick={() => {
+          if (item.name === "Logout") logoutF();
+          setIsMenuOpen(false);
+        }}
+      >
+        {item.name}
+      </Link>
+    </NavbarMenuItem>
+  );
+
+  return (
+    <Navbar
+      onMenuOpenChange={setIsMenuOpen}
+      isMenuOpen={isMenuOpen}
+      className={"drop-shadow-md"}
+    >
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Link href={"/"} className="font-bold text-inherit">
+            Medium
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        {filteredMenuItems.map((item, index) => (
+          <MenuItem key={`${item.name}-${index}`} item={item} />
+        ))}
+      </NavbarContent>
+      <NavbarMenu>
+        {filteredMenuItems.map((item, index) => (
+          <MenuItem key={`${item.name}-${index}`} item={item} />
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+
+export default MediumNavbar;
