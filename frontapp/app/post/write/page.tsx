c7996280f071971d2b9497b76d3578d738016ae1
jjy990412 <wjdwn282@gmail.com>
2024-01-03 11:04:10 +0900

    글 등록기능 추가

---
+++ frontapp/app/post/write/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import PrivateRoute from "@/components/PrivateRoute";
+import TextEditor from "@/components/TextEditor";
+import { instance } from "@/config/axiosConfig";
+import "@/styles/quillStyle.css";
+import { components } from "@/types/schema";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+
+const Page = () => {
+  const router = useRouter();
+  const [post, setPost] = useState<components["schemas"]["PostDto"]>({
+    published: true,
+  });
+
+  return (
+    <TextEditor
+      post={post}
+      setPost={setPost}
+      onSubmit={() => {
+        instance.post("/posts/write", post).then((res) => {
+          alert("등록되었습니다.");
+          router.back();
+        });
+      }}
+    />
+  );
+};
+
+export default PrivateRoute(Page);
