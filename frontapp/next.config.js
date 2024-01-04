e05d1c0ff77efabced95421730a64c4365a11f2a
jjy990412 <wjdwn282@gmail.com>
2024-01-04 12:48:54 +0900

    운영용 수정

--- frontapp/next.config.js
+++ frontapp/next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: "/api/v1/:path*",
-          destination: `http://localhost:8090/api/v1/:path*`,
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api/v1/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
